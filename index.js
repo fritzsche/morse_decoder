bf7e64d13db22cadbb70c9e0c06c74ed522d8a2e
Thomas Fritzsche <tf@noto.de>
2019-10-05 00:47:24 +0200

    Make Safari work

--- index.js
+++ index.js
@@ -7,16 +7,23 @@ class Decoder {
         // we should just use one ctx
         this._ctx = new (window.AudioContext || window.webkitAudioContext)();
         // getUserMedia need to be called on a navigator object
-        navigator.getUserMedia  = (navigator.getUserMedia || navigator.webkitGetUserMedia ||
-            navigator.mozGetUserMedia || navigator.msGetUserMedia)   
-        if (!navigator.getUserMedia ) alert("No UserMedia");
-        navigator.getUserMedia( { audio: true, video: false },
-            stream => {
-                var audioSource = this._ctx.createMediaStreamSource(stream);
-
-            },
+        navigator.getUserMedia = (navigator.getUserMedia || navigator.webkitGetUserMedia ||
+            navigator.mozGetUserMedia || navigator.msGetUserMedia)
+        var onGetUserMedia = (stream => {
+            var audioSource = this._ctx.createMediaStreamSource(stream);
+        });
+        var onGetUserMediaError = (
             err => { console.error('Error connecting: ' + err); }
         );
+        if (navigator.mediaDevices) {
+            navigator.mediaDevices.getUserMedia({ audio: true }).then(onGetUserMedia, onGetUserMediaError);
+        } else {
+            if (navigator.getUserMedia) {
+                navigator.getUserMedia({ audio: true, video: false }, onGetUserMedia, onGetUserMediaError);
+            } else {
+                alert("No User Media")
+            }
+        }
     }
 }
 
