65be23bd086090872526fe8ab000fb14735fb749
Thomas Fritzsche <tf@noto.de>
2019-10-06 14:57:29 +0200

    Update

--- index.js
+++ index.js
@@ -3,57 +3,56 @@ class Decoder {
 
 
     }
+
+    drawSpectrum(audioSource) {
+        var canvasElement = document.getElementById('spectrogram')
+        var gc = this.graphicContext = canvasElement.getContext("2d");
+        var analyserNode = this._ctx.createAnalyser();
+        audioSource.connect(analyserNode);
+        analyserNode.fftSize = 8192;
+        var fftData = new Float32Array(analyserNode.frequencyBinCount);
+
+        var graphicWidth = parseInt(getComputedStyle(canvasElement).width, 10);
+        var graphicHeight = parseInt(getComputedStyle(canvasElement).height, 10);
+
+        console.log(`GraphicsHeight: ${ graphicHeight }` );
+        console.log(`GraphicsWidth: ${ graphicWidth }`)
+
+        gc.fillStyle = '#000000';
+
+        gc.fillRect(0, 0, graphicWidth, graphicHeight);
+        var pixel = gc.createImageData(1, 1);
+        pixel.data[3] = 255;
+        var draw = ( () => {
+            var slideImage = gc.getImageData(0, 0, graphicWidth - 1, graphicHeight);
+            gc.putImageData(slideImage, 1, 0);
+
+            analyserNode.getFloatFrequencyData(fftData);
+            for (var i = 0; i < graphicHeight; ++i) {
+                var n = Math.min(Math.max((fftData[i] + 80) * 4, 0), 255);            
+                pixel.data[0] = n;
+                pixel.data[1] = n;
+                pixel.data[2] = n;
+                gc.putImageData(pixel, 0, graphicHeight - i);
+              }
+            requestAnimationFrame(draw);
+        } );
+        draw();
+    }
+
+
     play() {
         // we should just use one ctx
         this._ctx = new (window.AudioContext || window.webkitAudioContext)();
         // getUserMedia need to be called on a navigator object
         navigator.getUserMedia = (navigator.getUserMedia || navigator.webkitGetUserMedia ||
             navigator.mozGetUserMedia || navigator.msGetUserMedia)
-        var onGetUserMedia = (stream => {
-            var audioSource = this._ctx.createMediaStreamSource(stream);
-
-
-
-
-            var canvasElement = document.getElementById('spectrogram')
-            var gc = this.graphicContext = canvasElement.getContext("2d");
-            var analyserNode = this._ctx.createAnalyser();
-
-            audioSource.connect(analyserNode);
-
-            analyserNode.fftSize = 8192;
-            var fftData = new Float32Array(analyserNode.frequencyBinCount);
-
-            var graphicWidth = parseInt(getComputedStyle(canvasElement).width, 10);
-            var graphicHeight = parseInt(getComputedStyle(canvasElement).height, 10);
-
-
-            console.log(`GraphicsHeight: ${ graphicHeight }` );
-            console.log(`GraphicsWidth: ${ graphicWidth }`)
-
-            gc.fillStyle = '#000000';
-
-            gc.fillRect(0, 0, graphicWidth, graphicHeight);
-            var pixel = gc.createImageData(1, 1);
-            pixel.data[3] = 255;
-            var draw = ( () => {
-                var slideImage = gc.getImageData(0, 0, graphicWidth - 1, graphicHeight);
-                gc.putImageData(slideImage, 1, 0);
-
-                analyserNode.getFloatFrequencyData(fftData);
-                for (var i = 0; i < graphicHeight; ++i) {
-                    var n = Math.min(Math.max((fftData[i] + 80) * 4, 0), 255);            
-                    pixel.data[0] = n;
-                    pixel.data[1] = n;
-                    pixel.data[2] = n;
-                    gc.putImageData(pixel, 0, graphicHeight - i);
-                  }
-                requestAnimationFrame(draw);
-            } );
-            draw();
+        let onGetUserMedia = (stream => {
+             let audioSource = this._ctx.createMediaStreamSource(stream);
+             this.drawSpectrum(audioSource);
 
         });
-        var onGetUserMediaError = (err => { console.error('Error connecting: ' + err); });
+        let onGetUserMediaError = (err => { console.error('Error connecting: ' + err); });
         if (navigator.mediaDevices) {
             navigator.mediaDevices.getUserMedia({ audio: true, video: false }).then(onGetUserMedia, onGetUserMediaError);
         } else {
